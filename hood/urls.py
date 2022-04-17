7f78a54239305da676de831b406ea57b8f0f1ef5
kojwangbora <kojwangbora254@gmail.com>
2022-04-17 22:10:31 +0300

    add index template to render the view  fn

--- hood/urls.py
+++ hood/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, re_path, include
 from . import views
 
 urlpatterns=[
-    path('', views.welcome, name= 'welcome')
+    path('', views.index, name= 'index')
 ]
--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to the Hood')
+def index(request):
+    return render(request, 'index.html')
