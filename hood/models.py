6b9631e82c312ddf6cbbc9c7a66eaf9860a564dd
kojwangbora <kojwangbora254@gmail.com>
2022-04-18 18:45:18 +0300

    create form for updating user form

--- hood/models.py
+++ hood/models.py
@@ -5,30 +5,11 @@ from django.db.models.signals import post_save
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    profile_picture = CloudinaryField('images/', default='https://res.cloudinary.com/access-moringasch/image/upload/v1650274941/default_suwr8k.png')
-    name = models.CharField(max_length=100, blank=True)
-    bio = models.TextField(max_length=200, blank=True)
-
-    def __str__(self):
-        return f'{self.user.username} profile'
-
-    @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance)
-
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-        instance.profile.save()
-
 class NeighbourHood(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
     admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
-    hood_logo = CloudinaryField('images/')
+    hood_logo = CloudinaryField('image')
     description = models.TextField()
     health_tell = models.IntegerField(null=True, blank=True)
     police_number = models.IntegerField(null=True, blank=True)
@@ -45,3 +26,27 @@ class NeighbourHood(models.Model):
     @classmethod
     def find_neighborhood(cls, neighborhood_id):
         return cls.objects.filter(id=neighborhood_id)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    bio = models.TextField(max_length=254, blank=True)
+    profile_picture = CloudinaryField('image')
+    location = models.CharField(max_length=50, blank=True, null=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
+    
--- hood/views.py
+++ hood/views.py
@@ -3,6 +3,7 @@ from .forms import SignupForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from .models import NeighbourHood
+from .forms import UpdateProfileForm
 
 
 # Create your views here.
@@ -39,4 +40,5 @@ def profile(request, username):
     return render(request, 'profile.html')
 
 def edit_profile(request, username):
-    return render(request, 'editprofile.html')
+    form= UpdateProfileForm()
+    return render(request, 'editprofile.html', {'form': form})
