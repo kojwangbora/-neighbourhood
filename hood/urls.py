3b1f4321dce06a936ed5c959c3f7e3c7342b55cd
kojwangbora <kojwangbora254@gmail.com>
2022-04-18 21:05:08 +0300

    create view to allow user to join a certain hood

--- hood/urls.py
+++ hood/urls.py
@@ -7,7 +7,8 @@ urlpatterns=[
     path('register/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
     path('all-hoods/', views.hoods, name='hood'),
-
     path('profile/<username>', views.profile, name='profile'),
-    path('profile/<username>/edit/', views.edit_profile, name='edit-profile')
+
+    path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+    path('join_hood/<id>', views.join_hood, name='join_hood')
 ]
