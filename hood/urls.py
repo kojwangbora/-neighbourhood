0446bdf7a73855efed21ea4d348582e18a58db28
kojwangbora <kojwangbora254@gmail.com>
2022-04-18 10:43:06 +0300

    create signup form

--- hood/urls.py
+++ hood/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, re_path, include
 from . import views
 
 urlpatterns=[
-    path('', views.index, name= 'index')
+    path('', views.index, name= 'index'),
+    path('register/', views.SignupForm, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
 ]
