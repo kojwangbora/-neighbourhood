69022c510e437aa5d4f01a3b29c047b4c0ffdc66
kojwangbora <kojwangbora254@gmail.com>
2022-04-19 09:55:02 +0300

    create a view fn that prompts users to leave the hood

--- hood/urls.py
+++ hood/urls.py
@@ -7,9 +7,11 @@ urlpatterns=[
     path('register/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
     path('all-hoods/', views.hoods, name='hood'),
+    path('new-hood/', views.create_hood, name='new-hood'),
     path('profile/<username>', views.profile, name='profile'),
 
     path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
     path('join_hood/<id>', views.join_hood, name='join_hood'),
-    path('new-hood/', views.create_hood, name='new-hood'),
+    path('leave_hood/<hood_id>', views.leave_hood, name='leave-hood')
+    
 ]
