06675e758f314de148286c804b083f375a375793
kojwangbora <kojwangbora254@gmail.com>
2022-04-19 15:41:21 +0300

    add psot, members and results templates

--- hood/urls.py
+++ hood/urls.py
@@ -10,7 +10,12 @@ urlpatterns=[
     path('profile/<username>', views.profile, name='profile'),
 
     path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
-    path('join_hood/<id>', views.join_hood, name='join_hood'),
-    path('leave_hood/<hood_id>', views.leave_hood, name='leave-hood')
+    path('join_hood/<id>', views.join_hood, name='join-hood'),
+    path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
+    path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
+    
+    path('<hood_id>/new-post', views.create_post, name='post'),
+    path('<hood_id>/members', views.hood_members, name='members'),
+    path('search/', views.search_business, name='search'),
     
 ]
